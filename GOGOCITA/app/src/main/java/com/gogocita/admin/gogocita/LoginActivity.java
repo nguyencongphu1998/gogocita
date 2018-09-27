fd7b7094ab9d45030adf460d07639ea6ce16a636
NguyenHoang4875 <Contraicuagio97@gmail.com>
2018-09-27 17:56:01 +0700

    step 1-2-3-4

--- GOGOCITA/app/src/main/java/com/gogocita/admin/gogocita/LoginActivity.java
+++ GOGOCITA/app/src/main/java/com/gogocita/admin/gogocita/LoginActivity.java
@@ -9,7 +9,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.menu_partner);
     }
 
     public void btn_create(View v){
